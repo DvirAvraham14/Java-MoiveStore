1392c7c94119d6b5b050c54b88e7387b73338c70
Dvir Avraham <dviravraham14@gmail.com>
2023-05-08 19:59:39 +0300

    Fixed checkout

--- movie-app/src/components/Cart.js
+++ movie-app/src/components/Cart.js
@@ -1,10 +1,7 @@
 
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import {makeStyles} from '@mui/styles';
+import {Grid, Paper, Button, Typography} from '@mui/material';
 
 const useStyles = makeStyles((theme) => ({
     c_root: {
